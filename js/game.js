ab02552f0e8d09a02f4b56eda474500a7275a653
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-01-28 18:56:45 -0300

    Css / html

--- js/game.js
+++ js/game.js
@@ -1,4 +1,6 @@
 const grid = document.querySelector('.grid');
+const spanPlayer = document.querySelector('.player');
+const timer = document.querySelector('.timer');
 
 const characters = [
     'beth',
@@ -26,7 +28,7 @@ const checkEndGame = () => {
   const disabledCard = document.querySelectorAll('.disabled-card');
 
   if(disabledCard.length == 20){
-    alert('Parabéns, você Venceu!!!');
+    alert('Parabéns, você Venceu!');
   }
 }
 
@@ -103,4 +105,20 @@ const loadGame = () => {
   });
 }
 
-loadGame();
+
+const startTimer = () => {
+
+  this.loop = setInterval(() => {
+    const currentTime = +timer.innerHTML;
+    timer.innerHTML = currentTime + 1;
+  },1000);
+
+}
+
+window.onload = () => {
+  spanPlayer.innerHTML = localStorage.getItem('player');
+  startTimer();
+  loadGame();
+}
+
+
