005e6072f2ff86b89310b07061d21764e0af3e7e
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-01-27 22:26:21 -0300

    limpando JS

--- js/game.js
+++ js/game.js
@@ -1,20 +1,21 @@
 const grid = document.querySelector('.grid');
 
+const createElement = (tag, className) =>{
+  const element = document.createElement(tag);
+  element.className = className;
+  return element;
+}
+
 const createCard = () => {
 
-  const card = document.createElement('div');
-  const front = document.createElement('div');
-  const back = document.createElement('div');
-
-  card.className = 'card';
-  front.className = 'face front';
-  back.className = 'face back';
+  const card = createElement('div', 'card');
+  const front = createElement('div', 'face front');
+  const back = createElement('div', 'face back');
 
   card.appendChild(front);
   card.appendChild(back);
 
-  grid.appendChild(card);
+  return card;
 
 }
 
-createCard();
